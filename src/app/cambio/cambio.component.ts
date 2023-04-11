2daf908b4a505d5a1a03e14a429af8b5ef698491
ziluiz <luiz021205@gmail.com>
2023-04-11 10:46:23 -0300

    perdiaconta

--- src/app/cambio/cambio.component.ts
+++ src/app/cambio/cambio.component.ts
@@ -11,72 +11,96 @@ export class CambioComponent {
 moeda1 : number = 0;
 
 moeda2 : number = 0;
-cambio : any;
+
+moeda3 : number = 0;
+
+controle : any;
 
 constructor(){}
 
-execute(operacao){
+execute(controle : any){
 
-  switch(operacao){
-    case 'soma':
-      this.sum()
+  switch(controle){
+    case 'brlad':
+      this.brlad()
     break
 
-    case 'div':
-      this.div()
+    case 'brlcd':
+      this.brlcd()
     break
 
-    case 'mult':
-      this.mult()
+    case 'adcd':
+      this.adcd()
     break
 
-    case 'sub':
-      this.sub()
+    case 'adbrl':
+      this.adbrl()
     break
+
+    case 'cdad':
+      this.cdad()
+    break
+
+    case 'cdbrl':
+      this.cdbrl()
+    break
+
   }
-    }
+}
 
 
 
 //real - dolar americano
 brlad(){
 
-  this.moeda2 = this.moeda1 * 0.20
+  this.moeda3 = 1 *  0.20
+
+  this.moeda2 = this.moeda1 * this.moeda3
 
 }
 
 //real - dolar canadense
 brlcd(){
 
-  this.moeda2 = this.moeda1 * 0.27
+  this.moeda3 = 1 *  0.27
+
+  this.moeda2 = this.moeda1 * this.moeda3
 
 }
 
 //dolar americano - dolar canadense ****
 adcd(){
 
-  this.moeda2 = this.moeda1 * 1.35
+  this.moeda3 = 1 *  1.35
+
+  this.moeda2 = this.moeda1 * this.moeda3
 
 }
 
 //dolar americano - real
 adbrl(){
 
-  this.moeda2 = this.moeda1 * 5.07
+  this.moeda3 = 1 *  5.07
+
+  this.moeda2 = this.moeda1 * this.moeda3
 
 }
 
 //dolar canadense - dolar americano
 cdad(){
 
-  this.moeda2 = this.moeda1 * 0,74
+  this.moeda3 = 1 *  0.74
+
+  this.moeda2 = this.moeda1 * this.moeda3
 
 }
 
 //dolar canadense - real
 cdbrl(){
 
-  this.moeda2 = this.moeda1 * 3,75
+  this.moeda3 = 1 *  3.75
+
+  this.moeda2 = this.moeda1 * this.moeda3
 
 }
 
