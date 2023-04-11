d9d9625d5efa09b2ccaf831588a61a21aaf85b12
ziluiz <luiz021205@gmail.com>
2023-04-11 09:30:26 -0300

    quarto

--- src/app/cambio/cambio.component.ts
+++ src/app/cambio/cambio.component.ts
@@ -8,43 +8,50 @@ import { Component } from '@angular/core';
 export class CambioComponent {
 
 moeda1 : number = 0;
+
 moeda2 : number = 0;
-moeda3 : number = 0;
+
+constructor(){}
 
 //real - dolar americano
 brlad(){
 
- this.brlad = this.moeda1 / 5.07
+  this.moeda2 = this.moeda1 * 0.20
 
 }
 
 //real - dolar canadense
 brlcd(){
 
+  this.moeda2 = this.moeda1 * 0.27
 
 }
 
-//dolar americano - dolar canadense
+//dolar americano - dolar canadense ****
 adcd(){
 
+  this.moeda2 = this.moeda1 * 1.35
 
 }
 
 //dolar americano - real
 adbrl(){
 
+  this.moeda2 = this.moeda1 * 5.07
 
 }
 
 //dolar canadense - dolar americano
 cdad(){
 
+  this.moeda2 = this.moeda1 * 0,74
 
 }
 
 //dolar canadense - real
 cdbrl(){
 
+  this.moeda2 = this.moeda1 * 3,75
 
 }
 
